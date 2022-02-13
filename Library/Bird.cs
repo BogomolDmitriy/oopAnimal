b3957df6ba403c24ed16745ee728094fc79c7c8a
Anton Trotsenko <in.lake.district@gmail.com>
2022-02-13 19:20:26 +0200

    create new branch

---
+++ Library/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Bird
+    {
+        protected string _name;
+        protected string _kind;
+
+        protected string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public void WhatDoesTheBirdDo()
+        {
+            if (_name == "Giovanni Giorgio")
+            {
+                Console.WriteLine("He flies on another bird ");
+            }
+            else
+            {
+                Console.WriteLine("Bird must be Giovanni Giorgio");
+            }
+        } 
+    }
+}
